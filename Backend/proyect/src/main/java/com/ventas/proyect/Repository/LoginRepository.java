c38afe8df6b39b7c940ba39de91cdd7269299e8b
aportillo-2022361 <aportillo-2022361@kinal.edu.gt>
2026-04-15 00:50:42 -0600

    refactor:Se ha cambiado nombre de variable para evitar problemas

--- Backend/proyect/src/main/java/com/ventas/proyect/Repository/LoginRepository.java
+++ Backend/proyect/src/main/java/com/ventas/proyect/Repository/LoginRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LoginRepository extends JpaRepository<Login, Integer> {
-    Login findByUsuario(String username);
+    Login findByUsername(String username);
 }
