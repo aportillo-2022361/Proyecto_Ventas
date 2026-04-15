462d80cde145b2762eec74548ffe2ccf5ec032c6
aportillo-2022361 <aportillo-2022361@kinal.edu.gt>
2026-04-15 00:46:55 -0600

    feat:Se ha agregado la conexion de la home hacia otras vistas

--- Backend/proyect/src/main/java/com/ventas/proyect/Controller/HomeController.java
+++ Backend/proyect/src/main/java/com/ventas/proyect/Controller/HomeController.java
@@ -5,8 +5,33 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     @GetMapping("/home")
-    public String mostrarProfesor() {
+    public String mostrarHome() {
         return "Home";
+    }
+
+    @GetMapping("/clientes")
+    public String mostrarClientes() {
+        return "Clientes";
+    }
+
+    @GetMapping("/detalleVenta")
+    public String mostrarDetallesVentas() {
+        return "DetalleVentas";
+    }
+
+    @GetMapping("/productos")
+    public String mostrarProductos() {
+        return "Producto";
+    }
+
+    @GetMapping("/users")
+    public String mostrarUsers() {
+        return "Users";
+    }
+
+    @GetMapping("/ventas")
+    public String mostrarVentas() {
+        return "Ventas";
     }
 
 }
